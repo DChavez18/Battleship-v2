1db50834f13d43c7bd4b63927b263b0112ae2a5d
Derek Chavez <frrenose17@yahoo.com>
2024-01-12 13:44:03 -0700

    feat: add valid_placement? method for board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,45 @@
+require "./lib/board"
+require "./lib/cell"
+require "./lib/ship"
+
+RSpec.describe Board do
+  it "exists" do
+    board = Board.new
+
+    expect(board).to be_a(Board)
+  end
+
+  it "has cells" do
+    board = Board.new
+
+    expect(board.cells).to be_a(Hash)
+    expect(board.cells["A1"]).to be_a(Cell)
+  end
+
+  it "can validate coordinates" do
+    board = Board.new
+
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
+
+  it "can validate placements" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "B1", "C1"])).to eq(true)
+    expect(board.valid_placement?(submarine, ["A2", "B2"])).to eq(true)
+  end
+end
