d8af6783bc536b88a5e225a37bd7fe98381edddb
Derek Chavez <frrenose17@yahoo.com>
2024-01-19 11:20:17 -0700

    feat: add comp ships and player ships for game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,24 @@
 class Game
+  attr_reader :board, :ships, :player_ships
+
+  def initialize
+    @board = Board.new
+    @ships = []
+    @player_ships = []
+  end
+
+  def place_computer_ships
+    @ships << Ship.new("Cruiser", 3)
+    @ships << Ship.new("Submarine", 2)
+    @ships.each do |ship|
+      loop do
+        coordinates = @board.cells.keys.sample(ship.length)
+        if @board.valid_placement?(ship, coordinates)
+          @board.place(ship, coordinates)
+          break
+        end
+      end
+    end
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe Game do
 
     expect(game).to be_a(Game)
   end
+
+  it "has a board" do
+    game = Game.new
+
+    expect(game.board).to be_a(Board)
+  end
+
+  it "can have computer ships" do
+    game = Game.new
+
+    expect(game.ships).to eq([])
+  end
+
+  it "can have player ships" do
+    game = Game.new
+
+    expect(game.player_ships).to eq([])
+  end
+
+  it "can place computer ships" do
+    game = Game.new
+
+    game.place_computer_ships
+
+    expect(game.ships[0].length).to eq(3)
+    expect(game.ships[1].length).to eq(2)
+    expect(game.board.cells.values.count { |cell| cell.ship != nil }).to eq(5)    
+  end
 end
