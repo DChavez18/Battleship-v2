3979bc86d4820f5fe1455c7738d3db735cb7df24
Derek Chavez <frrenose17@yahoo.com>
2024-01-26 10:31:04 -0700

    spec: add testing for computer and player ships

--- lib/game.rb
+++ lib/game.rb
@@ -1,39 +1,18 @@
 class Game
-  attr_reader :board, :ships, :player_ships
+  attr_reader :player_board, 
+              :computer_board, 
+              :player_cruiser, 
+              :player_submarine, 
+              :computer_cruiser, 
+              :computer_submarine
 
   def initialize
-    @board = Board.new
-    @ships = []
-    @player_ships = []
-  end
-
-  def start
-    puts "Welcome to BATTLESHIP"
-    puts "Enter p to play. Enter q to quit."
-    print "> "
-    input = gets.chomp
-    if input == "p"
-      setup
-    elsif input == "q"
-      exit
-    else
-      puts "Invalid input."
-      start
-    end
-  end
-
-  def place_computer_ships
-    @ships << Ship.new("Cruiser", 3)
-    @ships << Ship.new("Submarine", 2)
-    @ships.each do |ship|
-      loop do
-        coordinates = @board.cells.keys.sample(ship.length)
-        if @board.valid_placement?(ship, coordinates)
-          @board.place(ship, coordinates)
-          break
-        end
-      end
-    end
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+    @player_board = Board.new
+    @computer_board = Board.new
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,31 +10,51 @@ RSpec.describe Game do
     expect(game).to be_a(Game)
   end
 
-  it "has a board" do
+  it "has a player board" do
     game = Game.new
 
-    expect(game.board).to be_a(Board)
+    expect(game.player_board).to be_a(Board)
   end
 
-  it "can have computer ships" do
+  it "has a computer board" do
     game = Game.new
 
-    expect(game.ships).to eq([])
+    expect(game.computer_board).to be_a(Board)
   end
 
-  it "can have player ships" do
+  it "has a player cruiser" do
     game = Game.new
 
-    expect(game.player_ships).to eq([])
+    expect(game.player_cruiser).to be_a(Ship)
+    expect(game.player_cruiser.name).to eq("Cruiser")
+    expect(game.player_cruiser.length).to eq(3)
+    expect(game.player_cruiser.health).to eq(3)
   end
 
-  it "can place computer ships" do
+  it "has a player submarine" do
     game = Game.new
 
-    game.place_computer_ships
+    expect(game.player_submarine).to be_a(Ship)
+    expect(game.player_submarine.name).to eq("Submarine")
+    expect(game.player_submarine.length).to eq(2)
+    expect(game.player_submarine.health).to eq(2)
+  end
 
-    expect(game.ships[0].length).to eq(3)
-    expect(game.ships[1].length).to eq(2)
-    expect(game.board.cells.values.count { |cell| cell.ship != nil }).to eq(5)    
+  it "has a computer cruiser" do
+    game = Game.new
+
+    expect(game.computer_cruiser).to be_a(Ship)
+    expect(game.computer_cruiser.name).to eq("Cruiser")
+    expect(game.computer_cruiser.length).to eq(3)
+    expect(game.computer_cruiser.health).to eq(3)
+  end
+
+  it "has a computer submarine" do
+    game = Game.new
+
+    expect(game.computer_submarine).to be_a(Ship)
+    expect(game.computer_submarine.name).to eq("Submarine")
+    expect(game.computer_submarine.length).to eq(2)
+    expect(game.computer_submarine.health).to eq(2)
   end
 end
